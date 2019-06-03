213269e77e9d185f80c30513d475e1916bd7a864
KENJILO <51340400+KENJILO@users.noreply.github.com>
2019-06-04 04:46:20 +0800

    Update gs-linebot.js

--- gs-linebot.js
+++ gs-linebot.js
@@ -7,7 +7,7 @@ function doGet(e){
   }
 
   var nowDatetime = new Date().toLocaleString();
-  userProperties.setProperty('temperatureText', nowDatetime  + "    [T] = " + temperature + " °C");
+  userProperties.setProperty('temperatureText', nowDatetime  + "temperature detecting[T] " + temperature + " °C");
   
   var returnText = temperature + " OK";
   var textOutput = ContentService.createTextOutput(returnText)
